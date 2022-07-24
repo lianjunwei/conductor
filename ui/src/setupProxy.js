70ff2c6380c3ac26d9819ca096eba197e5da2beb
lianjunwei <lianjunwei@didiglobal.com>
2022-07-24 15:39:20 +0800

    整合ES7

--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -1,5 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const target = process.env.WF_SERVER || "http://localhost:8080";
+// const target = process.env.WF_SERVER || "http://10.190.32.158:8080";
+const target = process.env.WF_SERVER || "http://10.190.51.224:8080";
 
 module.exports = function (app) {
   app.use(
